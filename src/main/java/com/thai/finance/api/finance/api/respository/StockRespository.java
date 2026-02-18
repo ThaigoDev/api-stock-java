daed623e5f03c23b07bd00104dca54118f74b74b
ThaigoDev <thaigodeveloper@gmail.com>
2026-02-18 12:54:14 -0300

    feat: login feature

--- src/main/java/com/thai/finance/api/finance/api/respository/StockRespository.java
+++ src/main/java/com/thai/finance/api/finance/api/respository/StockRespository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface StockRespository  extends JpaRepository<Stock, UUID> {
+    Stock findByProduct(UUID id);
 }
--- src/main/java/com/thai/finance/api/finance/api/services/StockMovementService.java
+++ src/main/java/com/thai/finance/api/finance/api/services/StockMovementService.java
@@ -4,10 +4,12 @@ import com.thai.finance.api.finance.api.dtos.stockMovementDTO.CreateStockMovemen
 import com.thai.finance.api.finance.api.dtos.stockMovementDTO.ResponseMovementStockDTO;
 import com.thai.finance.api.finance.api.dtos.stockMovementDTO.UpdateMovementStockDTO;
 import com.thai.finance.api.finance.api.entities.Product;
+import com.thai.finance.api.finance.api.entities.Stock;
 import com.thai.finance.api.finance.api.entities.Stock_Movement;
 import com.thai.finance.api.finance.api.mapper.StockMovementMapper;
 import com.thai.finance.api.finance.api.respository.ProductRepository;
 import com.thai.finance.api.finance.api.respository.StockMovementRepository;
+import com.thai.finance.api.finance.api.respository.StockRespository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,11 +23,13 @@ public class StockMovementService {
     private final StockMovementRepository stockMovementRepository;
     private final StockMovementMapper stockMovementMapper;
     private final ProductRepository productRepository;
+    private final StockRespository stockRespository;
 
-    public StockMovementService(StockMovementRepository stockMovementRepository, StockMovementMapper stockMovementMapper, ProductRepository productRepository) {
+    public StockMovementService(StockMovementRepository stockMovementRepository, StockMovementMapper stockMovementMapper, ProductRepository productRepository, StockRespository stockRespository) {
         this.stockMovementRepository = stockMovementRepository;
         this.stockMovementMapper = stockMovementMapper;
         this.productRepository = productRepository;
+        this.stockRespository = stockRespository;
     }
 
     public ResponseMovementStockDTO createStockMovement(CreateStockMovementDTO createStockMovementDTO) {
@@ -50,10 +54,18 @@ public class StockMovementService {
     public ResponseMovementStockDTO updateStockMovement( UUID id, UpdateMovementStockDTO updateMovementStockDTO) {
         Stock_Movement  stockMovementExist = stockMovementRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Stock Movement not found"));
         Product productFined =  productRepository.findById(updateMovementStockDTO.productId()).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Prodcut Not found"));
+        Stock stockFinded =  stockRespository.findByProduct(productFined.getId());
+
+        productFined.setInitialStock(productFined.getInitialStock() - updateMovementStockDTO.quantity());
+        stockFinded.setQuantityProduct(stockFinded.getQuantityProduct() - updateMovementStockDTO.quantity());
+
         stockMovementExist.setProduct(productFined);
         stockMovementExist.setType(updateMovementStockDTO.type());
         stockMovementExist.setQuantityMovement(updateMovementStockDTO.quantity());
         stockMovementExist.setUpdateAt(Instant.now());
+
+        productRepository.save(productFined);
+        stockRespository.save(stockFinded);
 
         var stockMovementCreated =  stockMovementMapper.entityStockMovementToResponseStockMovementDTO(stockMovementRepository.save(stockMovementExist));
        return stockMovementCreated;
